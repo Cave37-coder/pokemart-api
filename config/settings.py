efb62f14c79af1ae14defe1c5cfeb84907bd9ba6
Michael V <michaelv@pokebulk.co.za>
2026-04-18 20:34:54 +0200

    Use os.environ for DATABASE_URL

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
     )
 }
